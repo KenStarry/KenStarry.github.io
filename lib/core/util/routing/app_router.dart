7af9a8ed4acd7a9baded9666d7b8572bfb9d0613
KenStarry <starrycodes@gmail.com>
2025-01-10 21:39:29 +0300

    App Theme and Package installations

---
+++ lib/core/util/routing/app_router.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final GlobalKey<NavigatorState> rootNavigatorKey = GlobalKey<NavigatorState>();
+
+final GoRouter appRouter = GoRouter(
+  initialLocation: '/',
+    routes: []);
---
+++ lib/theme/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+/// Light Theme
+const Color primaryColor = Color(0xFF73eeb0);
+const Color secondaryColor = Color(0xFFED1C24);
+
+const Color backgroundColorDark = Color(0xFF2d2e32);
+const Color backgroundColorDarkAlt = Color(0xFF25262a);
+const Color whiteColor = Color(0xFFFFFFFF);
+const Color blackColor = Color(0xFF000000);
