84390920f354577697d7d65bbdd1e1c43d582962
KenStarry <starrycodes@gmail.com>
2025-01-11 14:01:50 +0300

    Bio Image Animations

--- lib/features/bio/presentation/components/bio_image.dart
+++ lib/features/bio/presentation/components/bio_image.dart
@@ -11,6 +11,34 @@ class BioImage extends StatefulWidget {
 }
 
 class _BioImageState extends State<BioImage> {
+  Widget _skillCircle(String asset,
+          {double top = 0,
+          double left = 0,
+          double bottom = 0,
+          required double width,
+          required double height,
+          double right = 0}) =>
+      Positioned(
+        left: left,
+        right: right,
+        top: top,
+        bottom: bottom,
+        child: UnconstrainedBox(
+          child: Container(
+            width: width,
+            height: height,
+            padding: const EdgeInsets.all(24),
+            decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.onSecondary,
+                shape: BoxShape.circle),
+            child: Image.asset('assets/images/$asset.png',
+                width: double.infinity,
+                height: double.infinity,
+                fit: BoxFit.contain),
+          ),
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -30,154 +58,47 @@ class _BioImageState extends State<BioImage> {
                       Theme.of(context).colorScheme.onSecondary,
                       Theme.of(context).colorScheme.onPrimary,
                     ])),
-          ),
+          )
+              .animate(autoPlay: true, delay: Duration(seconds: 1))
+              .fadeIn(duration: Duration(milliseconds: 300))
+              .moveX(duration: Duration(milliseconds: 300), begin: 50, end: 0),
         ),
 
         SizedBox(
-            width: double.infinity,
-            height: 600,
-            child: Stack(
-              children: [
-                Image.asset('assets/images/me.png',
+                width: double.infinity,
+                height: 600,
+                child: Image.asset('assets/images/me.png',
                     width: double.infinity,
                     height: double.infinity,
-                    fit: BoxFit.cover),
-                // Align(
-                //   alignment: Alignment.bottomCenter,
-                //   child: Container(
-                //     width: double.infinity,
-                //     height: 100,
-                //     decoration: BoxDecoration(
-                //         gradient: LinearGradient(
-                //             begin: Alignment.topCenter,
-                //             end: Alignment.bottomCenter,
-                //             colors: [
-                //           Theme.of(context)
-                //               .colorScheme
-                //               .onPrimary
-                //               .withValues(alpha: 0),
-                //           Theme.of(context)
-                //               .colorScheme
-                //               .onPrimary
-                //               .withValues(alpha: 0.2),
-                //           Theme.of(context)
-                //               .colorScheme
-                //               .onPrimary
-                //               .withValues(alpha: 0.4),
-                //           Theme.of(context)
-                //               .colorScheme
-                //               .onPrimary
-                //               .withValues(alpha: 0.6),
-                //           Theme.of(context)
-                //               .colorScheme
-                //               .onPrimary
-                //               .withValues(alpha: 0.8),
-                //           Theme.of(context).colorScheme.onPrimary,
-                //         ])),
-                //   ),
-                // )
-              ],
-            )),
+                    fit: BoxFit.cover))
+            .animate(autoPlay: true, delay: Duration(seconds: 1))
+            .fadeIn(duration: Duration(milliseconds: 300))
+            .moveX(duration: Duration(milliseconds: 300), begin: -50, end: 0),
 
-        Positioned(
-          left: 400,
-          right: 0,
-          top: 300,
-          bottom: 0,
-          child: UnconstrainedBox(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(500),
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                child: Container(
-                  width: 120,
-                  height: 120,
-                  padding: const EdgeInsets.all(24),
-                  decoration: BoxDecoration(
-                      color: Theme.of(context)
-                          .colorScheme
-                          .onSecondary
-                          .withValues(alpha: 0.3),
-                      shape: BoxShape.circle),
-                  child: Image.asset('assets/images/flutter.png',
-                      width: double.infinity,
-                      height: double.infinity,
-                      fit: BoxFit.contain),
-                ),
-              ),
-            ),
-          )
-              .animate(
-                  autoPlay: true,
-                  onPlay: (controller) => controller.repeat(reverse: true))
-              .scale(
-                  duration: Duration(seconds: 1),
-                  begin: Offset(1, 1),
-                  end: Offset(1.1, 1.1)),
-        ),
-        Positioned(
-          left: 0,
-          right: 450,
-          top: 150,
-          bottom: 0,
-          child: UnconstrainedBox(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(500),
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                child: Container(
-                  width: 80,
-                  height: 80,
-                  padding: const EdgeInsets.all(24),
-                  decoration: BoxDecoration(
-                      color: Theme.of(context)
-                          .colorScheme
-                          .onSecondary
-                          .withValues(alpha: 0.3),
-                      shape: BoxShape.circle),
-                  child: Image.asset('assets/images/kotlin.png',
-                      width: double.infinity,
-                      height: double.infinity,
-                      fit: BoxFit.contain),
-                ),
-              ),
-            ),
-          )
-              .animate(
-                  autoPlay: true,
-                  onPlay: (controller) => controller.repeat(reverse: true))
-              .scale(
-                  duration: Duration(seconds: 1),
-                  begin: Offset(1, 1),
-                  end: Offset(1.1, 1.1)),
-        ),
-        Positioned(
-          left: 450,
-          right: 0,
-          top: 0,
-          bottom: 200,
-          child: UnconstrainedBox(
-            child: Container(
-              width: 60,
-              height: 60,
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.onSecondary,
-                  shape: BoxShape.circle),
-              child: Image.asset('assets/images/firebase.png',
-                  width: double.infinity,
-                  height: double.infinity,
-                  fit: BoxFit.contain),
-            ),
-          )
-              .animate(
-                  autoPlay: true,
-                  onPlay: (controller) => controller.repeat(reverse: true))
-              .scale(
-                  duration: Duration(seconds: 1),
-                  begin: Offset(1, 1),
-                  end: Offset(1.1, 1.1)),
-        ),
+        /// Flutter
+        _skillCircle('flutter', width: 120, height: 120, left: 400, top: 300)
+            .animate(autoPlay: true, delay: Duration(milliseconds: 1500))
+            .fadeIn(duration: Duration(milliseconds: 300))
+            .scale(duration: Duration(milliseconds: 300)),
+
+        /// Kotlin
+        _skillCircle('kotlin', width: 80, height: 80, right: 450, top: 150)
+            .animate(autoPlay: true, delay: Duration(milliseconds: 1300))
+            .fadeIn(duration: Duration(milliseconds: 300))
+            .scale(duration: Duration(milliseconds: 300)),
+        // .animate(
+        //     autoPlay: true,
+        //     onPlay: (controller) => controller.repeat(reverse: true))
+        // .scale(
+        //     duration: Duration(seconds: 1),
+        //     begin: Offset(1, 1),
+        //     end: Offset(1.1, 1.1)),
+
+        /// Firebase
+        _skillCircle('firebase', width: 80, height: 80, left: 450, bottom: 200)
+            .animate(autoPlay: true, delay: Duration(milliseconds: 1700))
+            .fadeIn(duration: Duration(milliseconds: 300))
+            .scale(duration: Duration(milliseconds: 300)),
       ],
     );
   }
